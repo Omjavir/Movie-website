c6d1ef6d5db1497a5c5af1196a01ef8016f9e140
Om Suresh Javir <javirom07042003@gmail.com>
2023-02-03 11:04:35 +0530

    Improved search bar

--- src/Pages/Search.js
+++ src/Pages/Search.js
@@ -3,12 +3,17 @@ import SearchIcon from '@mui/icons-material/Search';
 import axios from 'axios';
 import CustomPagination from '../Components/CustomPagination';
 import Card from '../Components/Card';
+import CallMadeIcon from '@mui/icons-material/CallMade';
 
 const Search = () => {
   const [content, setContent] = useState([]);
   const [numOfPages, setNumOfPages] = useState();
   const [page, setPage] = useState(1);
   const [query, setQuery] = useState(" ");
+  const value1 = "Pathaan";
+  const value2 = "Love";
+  const value3 = "Fifty shades";
+
 
   const fetchSearch = async () => {
     const data = await axios.get(`https://api.themoviedb.org/3/search/movie?api_key=9ab656c6f1596990ec8a11ec1801b101&language=en-US&query=${query}&page=${page}&include_adult=false`)
@@ -18,20 +23,34 @@ const Search = () => {
   }
 
   useEffect(() => {
-    fetchSearch();
-  }, [query, page])
+    const timer = setTimeout(() => {
+      fetchSearch();
+      // console.log("Query Length : ", query.length);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, [query]);
+
+  // useEffect(() => {
+  //   fetchSearch();
+  // }, [query, page])
 
   return (
-    <div className='mb-28 justify-center m-auto'>
-      <div className="flex items-center justify-center m-5">
+    <div className='mb-22 justify-center m-auto'>
+      <div className="items-center flex justify-center m-5">
         <div className="flex rounded border-2 border-blue-500">
-          <input type="text" className="px-4 py-2 w-80 sm:w-96" placeholder="Search..." onChange={(e) => setQuery(e.target.value)} />
-          <button className="flex items-center justify-center px-4 border-l">
+          <input type="text" className="sm:px-4 p-2 w-72 sm:w-96" placeholder="Search..." onChange={(e) => setQuery(e.target.value)} />
+          <button className="flex items-center border-x-2 border-blue-500 justify-center px-4 border-l">
             <SearchIcon />
           </button>
         </div>
       </div>
-      <div className='flex flex-wrap justify-center mb-18'>
+      {query.length === 1 &&
+        <div className="suggestions flex flex-col mt-2 cursor-pointer">
+          <span className='mx-2 my-1 border-b-2' onClick={() => setQuery(value1)}><CallMadeIcon />{value1}</span>
+          <span className='mx-2 my-1 border-b-2' onClick={() => setQuery(value2)}><CallMadeIcon />{value2}</span>
+          <span className='mx-2 my-1 border-b-2' onClick={() => setQuery(value3)}><CallMadeIcon />{value3}</span>
+        </div>}
+      <div className='flex flex-wrap justify-center mb-12'>
         {content && content.map((c) => {
           return <Card
             key={c.id}
